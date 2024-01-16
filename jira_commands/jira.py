c6ff58b8ea3e91681559ad75798ca922377179c1
Joe Block <jblock@zscaler.com>
2024-01-16 16:48:11 -0700

    Start adding label-related commands to JiraTool
    Makefile:
    - Update format handling stanzas in Makefile
    
    JiraTool Class:
    - Add add_ticket_labels()
    - Add remove_ticketlabel()
    - Add get_issue_labels()
    
    Signed-off-by: Joe Block <jblock@zscaler.com>

--- jira_commands/jira.py
+++ jira_commands/jira.py
@@ -1224,3 +1224,64 @@ class JiraTool:
 
         logging.debug("Set data[%s] to %s", custom_field, fields[custom_field])
         return fields
+
+    def add_issue_label(self, ticket: str = None, labels=None):
+        """
+        Add a label or labels to a ticket.
+
+        Args:
+            ticket: what ticket to add the label(s) to
+            labels: either a str or a list of str
+        """
+        if not ((type(labels) is list) or (type(labels) is str)):
+            raise ValueError("labels must be a str or a list of strings")
+        issue = self.get_ticket(ticket=ticket)
+        if type(labels) == str:
+            logging.info(f"Convert {labels} to list")
+            labels = [labels]
+        if type(labels) == list:
+            logging.info(f"labels = {labels}")
+            for l in labels:
+                if type(l) is str:
+                    # JIRA is slow, so eliminate unnecessary calls to the API
+                    if l not in issue.fields.labels:
+                        issue.fields.labels.append(l)
+                else:
+                    raise ValueError(
+                        f"Attempted to add labels {labels} from {ticket}, but {l} is not type str"
+                    )
+        return issue.update(fields={"labels": issue.fields.labels})
+
+    def get_issue_labels(self,ticket:str=None):
+        '''
+        Get labels for an issue
+        Args:
+            ticket: str of ticket to get labels for
+        '''
+        issue = self.get_ticket(ticket=ticket)
+        return issue.fields.labels
+    def remove_issue_label(self, ticket: str = None, label=None):
+        """
+        Remove a label or list of labels from an issue
+
+        Args:
+            ticket: what ticket to remove the label(s) from
+            labels: either a str or a list of str
+        """
+        if not ((type(label) is list) or (type(label) is str)):
+            raise ValueError("label must be a str or a list of strings")
+        issue = self.get_ticket(ticket=ticket)
+        if type(label) == str:
+            label = [label]
+
+        if type(label) == list:
+            for l in label:
+                if type(l) is str:
+                    # if the label isn't present, we don't want to error
+                    if l in issue.fields.labels:
+                        issue.fields.labels.remove(l)
+                else:
+                    raise ValueError(
+                        f"Attempted to remove labels {label} from {ticket}, but {l} is not type str"
+                    )
+        return issue.update(fields={"labels": issue.fields.labels})
